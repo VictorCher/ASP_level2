7d915a3318139047f6e9ceec1770fdfddf1314cf
Виктор Чернышов <192.168.127.1@mail.ru>
2019-09-17 11:18:09 +0300

    ДЗ №1

---
+++ UI/WebStore/Controllers/WebApiTestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Api;
+
+namespace WebStore.Controllers
+{
+    public class WebApiTestController : Controller
+    {
+        private readonly IValuesService _Values;
+
+        public WebApiTestController(IValuesService Values) => _Values = Values;
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _Values.GetAsync();
+
+            return View(values);
+        }
+    }
+}
